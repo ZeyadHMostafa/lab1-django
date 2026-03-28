fce59afbb3247da3c8ae91666087cd1d9a0b5272
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-03-28 09:16:23 +0200

    added update capablities

--- studentmanager/urls.py
+++ studentmanager/urls.py
@@ -3,11 +3,18 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	
 	path('students/', views.manage_students, name='manage_students'),
+	path('students/update/<int:student_id>/', views.update_student, name='update_student'),
 	path('students/delete/<int:student_id>/', views.delete_student, name='delete_student'),
+
 	path('subjects/', views.manage_subjects, name='manage_subjects'),
+	path('subjects/update/<int:pk>/', views.update_subject, name='update_subject'),
 	path('subjects/delete/<int:subject_id>/', views.delete_subject, name='delete_subject'),
+
 	path('grades/', views.manage_grades, name='manage_grades'),
+	path('grades/update/<int:pk>/', views.update_grade, name='update_grade'),
 	path('grades/delete/<int:grade_id>/', views.delete_grade, name='delete_grade'),
+
 	path('contact/', views.contact_us, name='contact_us'),
 ]
--- studentmanager/views.py
+++ studentmanager/views.py
@@ -7,7 +7,6 @@ def home(request):
 	return render(request, 'studentmanager/home.html')
 
 def manage_students(request):
-	# Handle Adding a Student
 	if request.method == "POST":
 		form = StudentForm(request.POST, request.FILES)
 		if form.is_valid():
@@ -16,12 +15,22 @@ def manage_students(request):
 	else:
 		form = StudentForm()
 
-	# Get all students for the table
 	students = Student.objects.all()
 	return render(request, 'studentmanager/manage_students.html', {
 		'form': form,
 		'students': students
 	})
+
+def update_student(request, student_id):
+	student = get_object_or_404(Student, student_id=student_id)
+	if request.method == "POST":
+		form = StudentForm(request.POST, request.FILES, instance=student)
+		if form.is_valid():
+			form.save()
+			return redirect('manage_students')
+	else:
+		form = StudentForm(instance=student)
+	return render(request, 'studentmanager/update_item.html', {'form': form, 'title': 'Update Student'})
 
 def delete_student(request, student_id):
 	student = get_object_or_404(Student, student_id=student_id)
@@ -38,6 +47,17 @@ def manage_subjects(request):
 		form = SubjectForm()
 	subjects = Subject.objects.all()
 	return render(request, 'studentmanager/manage_subjects.html', {'form': form, 'subjects': subjects})
+
+def update_subject(request, pk):
+	subject = get_object_or_404(Subject, pk=pk)
+	if request.method == "POST":
+		form = SubjectForm(request.POST, instance=subject)
+		if form.is_valid():
+			form.save()
+			return redirect('manage_subjects')
+	else:
+		form = SubjectForm(instance=subject)
+	return render(request, 'studentmanager/update_item.html', {'form': form, 'title': 'Update Subject'})
 
 def delete_subject(request, subject_id):
 	subject = get_object_or_404(Subject, id=subject_id)
@@ -62,6 +82,17 @@ def manage_grades(request):
 		'form': form,
 		'grades': grades
 	})
+
+def update_grade(request, pk):
+	grade = get_object_or_404(Grade, pk=pk)
+	if request.method == "POST":
+		form = GradeForm(request.POST, instance=grade)
+		if form.is_valid():
+			form.save()
+			return redirect('manage_grades')
+	else:
+		form = GradeForm(instance=grade)
+	return render(request, 'studentmanager/update_item.html', {'form': form, 'title': 'Update Grade'})
 
 def delete_grade(request, grade_id):
 	grade = get_object_or_404(Grade, id=grade_id)
